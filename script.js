43298d57c988d3ad889ef3f92a710fe1c1f7100d
Ramon <ramonluciolinhares@gmail.com>
2020-08-16 14:28:56 -0300

    Refactoring the calculate function

--- script.js
+++ script.js
@@ -3,19 +3,12 @@ const keys = calculator.querySelector('.calculator-keys')
 const display = calculator.querySelector('.calculator-display')
 
 const calculate = (n1, operator, n2) => {
-    let result = ''
-    
-    if (operator === 'add') {
-      result = parseFloat(n1) + parseFloat(n2)
-    } else if (operator === 'subtract') {
-      result = parseFloat(n1) - parseFloat(n2)
-    } else if (operator === 'multiply') {
-      result = parseFloat(n1) * parseFloat(n2)
-    } else if (operator === 'divide') {
-      result = parseFloat(n1) / parseFloat(n2)
-    }
-    
-    return result
+    const firstNum = parseFloat(n1)
+    const secondNum = parseFloat(n2)
+    if (operator === 'add') return firstNum + secondNum
+    if (operator === 'subtract') return firstNum - secondNum
+    if (operator === 'multiply') return firstNum * secondNum
+    if (operator === 'divide') return firstNum / secondNum
 }
 
 keys.addEventListener('click', e => {
@@ -106,5 +99,5 @@ keys.addEventListener('click', e => {
             calculator.dataset.modValue = secondValue
             calculator.dataset.previousKeyType = 'calculate'
           }
-        }
-    })
+    }
+})
